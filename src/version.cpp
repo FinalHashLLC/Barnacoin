e1d72f5f7daaf38d312c42391fadb3a8b101492d
Ahmed Bodiwala <ahmed_bodi@msn.com>
2014-11-28 20:10:40 -0500

    Barnacoin Version String

--- src/version.cpp
+++ src/version.cpp
@@ -9,7 +9,7 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both barnacoind and barnacoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("Satoshi");
+const std::string CLIENT_NAME("Barnacoin");
 
 // Client version number
 #define CLIENT_VERSION_SUFFIX   "-beta"
